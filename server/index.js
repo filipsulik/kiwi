67c1863de37a1b9fbec27b8c19afcbb5c43e864c
Filip Sulik <fiko@vestberry.com>
2021-05-15 12:33:07 +0200

    database init

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const sequelize = require('./database')
+
+sequelize.sync().then(() => console.log('database ready'))
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
-app.get("/api", (req, res) => {
-  res.json({ message: "test" });
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
